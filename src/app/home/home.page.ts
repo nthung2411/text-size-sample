a6a47cbafa96223eb9785a69e22685f143a9e686
Robert Hung Nguyen <hung.nguyentan@axonactive.com>
2021-02-25 17:04:04 +0700

    display computed size

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
 
-  constructor() {}
+  @ViewChild('size18') size18;
+  @ViewChild('size16') size16;
+  @ViewChild('size14') size14;
+  @ViewChild('size12') size12;
+
+  public size18Computed: string;
+  public size16Computed: string;
+  public size14Computed: string;
+  public size12Computed: string;
+
+  ngAfterViewInit(): void {
+    this.size18Computed = window.getComputedStyle(this.size18.nativeElement).fontSize;
+    this.size16Computed = window.getComputedStyle(this.size16.nativeElement).fontSize;
+    this.size14Computed = window.getComputedStyle(this.size14.nativeElement).fontSize;
+    this.size12Computed = window.getComputedStyle(this.size12.nativeElement).fontSize;
+  }
 
 }
